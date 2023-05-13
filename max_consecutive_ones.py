4adeca064e5919f9555e8271acd49bd81a5e5c8c
imparthv <imparthvasdewani@gmail.com>
2023-05-13 11:00:55 +0530

    Solution for Leetcode Problem Max Consecutive Ones

---
+++ max_consecutive_ones.py
@@ -0,0 +1,14 @@
+#Max Consecutive Ones
+def findMaxConsecutiveOnes(nums):
+    nums.append(0)
+    count_one_list = []
+    temp_count = 0
+    for item in nums:
+        if item == 1:
+            temp_count += 1
+        else: 
+            count_one_list.append(temp_count)
+            temp_count = 0
+    return max(count_one_list)
+
+print(findMaxConsecutiveOnes([1,0,1,1,0,1]))
